bb5fb270d36e8ddf063f798d7d216887b6903648
TheDarkOne1211 <praveens1997@gmail.com>
2019-01-01 16:19:24 -0500

    set up the projects page

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,10 @@
 var timelinetextBox = document.getElementsByClassName("timeline-text");
 var timeline = document.getElementsByClassName("timeline");
+var coll = document.getElementsByClassName("collapsible");
 var screenwidth = screen.width;
 readjustHeight();
 timelinetextReveal();
+activateCollapsible();
 
 console.log(screenwidth);
 function readjustHeight() {
@@ -20,4 +22,20 @@ function timelinetextReveal() {
   sr.reveal(timelinetextBox[3], { reset: true, duration: 1000 });
   sr.reveal(timelinetextBox[4], { reset: true, duration: 1000 });
   readjustHeight();
+}
+
+function activateCollapsible() {
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
 }

ff2a60bcecbf20e3b10f88b2da517d1019715e96
TheDarkOne1211 <praveens1997@gmail.com>
2019-01-01 00:20:47 -0500

    added about page with animations and  contact page

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,23 @@
-var responsiveNavbtn = document.getElementsByClassName("nav-btn");
+var timelinetextBox = document.getElementsByClassName("timeline-text");
+var timeline = document.getElementsByClassName("timeline");
+var screenwidth = screen.width;
+readjustHeight();
+timelinetextReveal();
 
-function changeNavBtnBackground() {
-  responsiveNavbtn.style.background = "orange";
+console.log(screenwidth);
+function readjustHeight() {
+  if (screenwidth <= 764) {
+    timelinetextBox[2].setAttribute("style", "height: 220px !important");
+    timelinetextBox[4].setAttribute("style", "height: 180px !important");
+  }
+}
+
+function timelinetextReveal() {
+  window.sr = ScrollReveal();
+  sr.reveal(timelinetextBox[0], { reset: true, duration: 1000 });
+  sr.reveal(timelinetextBox[1], { reset: true, duration: 1000 });
+  sr.reveal(timelinetextBox[2], { reset: true, duration: 1000 });
+  sr.reveal(timelinetextBox[3], { reset: true, duration: 1000 });
+  sr.reveal(timelinetextBox[4], { reset: true, duration: 1000 });
+  readjustHeight();
 }

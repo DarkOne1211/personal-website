0b6ec06851ad9e77f82869490a21b0907393d99f
TheDarkOne1211 <praveens1997@gmail.com>
2019-01-04 00:00:01 -0500

    cover page for 'project' and changed 'about' page

--- source/js/app.js
+++ source/js/app.js
@@ -3,7 +3,6 @@ var timeline = document.getElementsByClassName("timeline");
 var coll = document.getElementsByClassName("collapsible");
 var screenwidth = screen.width;
 readjustHeight();
-timelinetextReveal();
 activateCollapsible();
 
 console.log(screenwidth);
@@ -12,16 +11,6 @@ function readjustHeight() {
     timelinetextBox[2].setAttribute("style", "height: 220px !important");
     timelinetextBox[4].setAttribute("style", "height: 180px !important");
   }
-}
-
-function timelinetextReveal() {
-  window.sr = ScrollReveal();
-  sr.reveal(timelinetextBox[0], { reset: true, duration: 1000 });
-  sr.reveal(timelinetextBox[1], { reset: true, duration: 1000 });
-  sr.reveal(timelinetextBox[2], { reset: true, duration: 1000 });
-  sr.reveal(timelinetextBox[3], { reset: true, duration: 1000 });
-  sr.reveal(timelinetextBox[4], { reset: true, duration: 1000 });
-  readjustHeight();
 }
 
 function activateCollapsible() {
